ce38324f8a533d6f0b705569c3404f652e0a6775
kinglob <der1010101013@gmail.com>
2026-05-25 09:40:16 +1000

    making build options for windows & linux (and nixos btw)
    Signed-off-by: kinglob <der1010101013@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Nix flake for testing EPBBCode on NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        builtins.listToAttrs (
+          map (system: {
+            name = system;
+            value = f system;
+          }) systems
+        );
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          # legacy doesnt mean old btw, just means
+          # ALL the stuff in the packages (including dev
+          # stuff)
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              luau
+              just
+              jq
+            ];
+          };
+        }
+      );
+    };
+}
